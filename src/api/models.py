6b052c2fab7057aeeae2e8a9b604a2b06dd74a9a
MJRod23 <m.rodriguez4260@gmail.com>
2023-02-01 02:09:13 +0000

    new backend

--- src/api/models.py
+++ src/api/models.py
@@ -4,8 +4,11 @@ db = SQLAlchemy()
 
 #CREATE YOUR DATABASE TABLES HERE
 class User(db.Model):
-    __tablename__ = "user"
+    __tablename__ = "User"
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), nullable=False, unique=True)
+    email = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
 
     def __repr__(self):
         return f'<User {self.id}>'
@@ -13,5 +16,38 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "email": self.email,
+            "username": self.username
+            # do not serialize the password, its a security breach
+        }
+
+
+class Recipe(db.Model):
+    __tablename__ = "recipe"
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), primary_key=True)
+    api_id = db.Column(db.Integer, nullable=False, unique=True)
+    carbs = db.Column(db.String(250), nullable=False)
+    fat = db.Column(db.String(250), nullable=False)
+    protein = db.Column(db.String(250), nullable=False)
+    title = db.Column(db.String(250), nullable=False)
+    calories = db.Column(db.String(250), nullable=False)
+    image = db.Column(db.String(250), nullable=False)
+    user=db.relationship("User")
+
+
+    def __repr__(self):
+        return f'<Recipe {self.user_id}>'
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "api_id": self.api_id,
+            "fat": self.fat,
+            "protein": self.protein,
+            "title": self.title,
+            "calories": self.calories,
+            "image": self.image,
+            "carbs": self.carbs,
+
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -13,3 +13,37 @@ def handle_hello():
     response_body.headers.add("Access-Control-Allow-Origin", "*")
     return response_body, 200
 
+@api.route("/signup", methods=["POST"])
+@cross_origin()
+def signup():
+    if request.method == 'POST':
+        email = request.json.get('email', None)
+        username = request.json.get('username', None)
+        password = request.json.get('password', None)
+
+        if not email:
+            return 'Email is required', 401
+        if not username:
+            return 'Username is required', 401
+        if not password:
+            return 'Password is required', 401
+        
+        email_query = User.query.filter_by(email=email).first()
+        if email_query:
+            return 'This email already exists' , 402
+        user = User()
+        user.email = email
+        user.username = username
+        user.password = password
+        user.is_active = True
+        print(user)
+        db.session.add(user)
+        db.session.commit()
+
+        response = {
+            'msg': 'User added successfully',
+            'email': email,
+            'username': username
+        }
+        return jsonify(response), 200
+
